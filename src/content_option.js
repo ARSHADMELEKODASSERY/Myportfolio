a1a3938c1ae013f746e6e6e25640ac7f2307487e
arshad <arshad@frugalscientific.com>
2025-02-18 18:05:42 +0530

    updated

---
+++ src/content_option.js
@@ -0,0 +1,195 @@
+const logotext = "ARSHAD.";
+const meta = {
+  title: "Arshad MK",
+  description:
+    "I’m ARSHAD MK data scientist _ Full stack devloper,currently working in Frugal Scientific private Ltd",
+};
+
+const introdata = {
+  title: "I’m Arshad MK  and I'm a",
+  animated: {
+    first: "Web Developer",
+    second: "",
+    third: "Full Stack Developer",
+  },
+  description: "",
+  your_img_url:
+    "https://media-hosting.imagekit.io//6b7be8359adf49cd/arshadportfolioBG.png?Expires=1834383781&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=l0NAmx6EN1Ih8NU-3u1mC~KCVpNhnRBXB2RK1cg4tOU0XI2qrMnOk4fYDsoUOCIZMRI~rX9aDZiLXi415F19AuvtBcl4h2VzbYOgqkO1IFkIVZTHK8vqL03NrTguqjXdpy1VhgOWhsh~77hUFJTh5lrC6sdGoHxrDcEy1kruuH4FR0VQwdHRw~FXLZ-sv7wgVmFru61RFLF3dPIty6LPxQvlNFvd-YprxX41oOyor6zMpB2FPm3MdDwWyOlVmIVtXYQR95HNzvwadQ62izKUFUCLjzfUCEK9jdThMg0gT5u6J4Nf35KiFVPwrzENz-4X1CeWUwpwXlKzyB0bB-YaNg__",
+};
+
+const dataabout = {
+  title: "about my self",
+  aboutme:
+    "A passionate and dedicated FullStack developer with graduate, I have completed my postgraduate degree in Computer Science at R.V.S College of Arts and Science, Coimbatore, specialized in Data Science and Full Stack Development.With a strong foundation in front-end and back-end technologies, I aspire to become a Full Stack Developerwith great knowledge. My positive attitude, reliability, and commitment to learning make me a strong team player and a valuable asset in any development environment",
+};
+const worktimeline = [
+  {
+    jobtitle: "Associative Product Engineer",
+    where: "Frugal Scientific Private Ltd",
+    date: "Apr 2024 - Present",
+  },
+  {
+    jobtitle: "FullStack Developer Intern",
+    where: "FullStack Developer Academy",
+    date: "Aug 2023 - Apr 2024",
+  },
+  {
+    jobtitle: "Junior Software Engineer",
+    where: "PS Netsuite Solutions",
+    date: "Apr 2023 - Aug 2023",
+  },
+];
+const qualification = [
+  {
+    name: "MSc Computer Science",
+    school: "RVS College of Arts And Science,Coimbatore",
+    year: 2023,
+  },
+  {
+    name: "BSc Computer Science",
+    school: "CAS Muthuvallur donated by I.H.R.D",
+    year: 2019,
+  },
+  {
+    name: "+2 (Computer Science)",
+    school: "G.H.S.S Peruvallur",
+    year: 2016,
+  },
+];
+const skills = [
+  {
+    name: "Html , Css",
+    value: 90,
+  },
+  {
+    name: "Tableau",
+    value: 60,
+  },
+  {
+    name: "Jquery",
+    value: 85,
+  },
+  {
+    name: "Microsoft Excel",
+    value: 70,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "Statistics",
+    value: 65,
+  },
+  {
+    name: "React",
+    value: 70,
+  },
+  {
+    name: "Data Visualization using R",
+    value: 50,
+  },
+  {
+    name: "Mongo Db",
+    value: 85,
+  },
+  {
+    name: "Data Visulizing using python",
+    value: 50,
+  },
+  {
+    name: "Node",
+    value: 70,
+  },
+  {
+    name: "Sql",
+    value: 70,
+  },
+];
+
+const services = [
+  {
+    title: "AI-Summit Completion certificate.",
+    description:
+      "AI-Summit Completion certificate by WoxenUniversity. on (27th-28th Oct 2021)",
+  },
+  {
+    title: "Student Developement Programme",
+    description: "12 Days Student Development Program (SDP) by INFOSYS.",
+  },
+  {
+    title: "Web Developement Bootcamp",
+    description: "Web Developement Bootcamp by Codekaro on 13 Nov 2022.",
+  },
+  {
+    title: "Data Aalytic Workshop",
+    description: "Data Aalytic Workshop from Jobaaj Learing on 22 Nov 202.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://media-hosting.imagekit.io//8049e5c6c5454ee4/ship.jpg?Expires=1834398362&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=hasD~05CNzRVx2TWJy9XNcs-VgrgMm3Sh-0Oi3Fajh5w8COqDq6CEejYlcMLLzjO9ixHGRIsXuuPluqkk9FmUhRHtY2Tqqw2xhej6A2jJ6jszLgMss~YlasBXYzx4Jp3QZ8~3Yko4FDNE-r0ivnF0bhoBLjVLLk6gDywLX6mYbYd8I-BFGyWV-XaN6lHfoJrM1pF9qcpEjcH8HKYIrZoDpRGRhLHLcgpQj8C-gr7fUfuOx7vWqpA9MGZO6XZr1KGnbFX429AgD2hH6zN6NsyhQcA~f0ijj-Tz20xPN9lfUv57Ep~iHabNgfhEkLg9m-uKbbT2~eVooPQXhXVKDcYEQ__",
+    description:
+      "Inclips is the platform for seamless marine survey management.",
+    link: "https://inclipsmarine.com/",
+  },
+  {
+    img: "https://media-hosting.imagekit.io//ab8cc29ab8334554/sarga.png?Expires=1834402996&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=BNb2JroTqPf03~hPvn6HkgrjqgbUrOlhDbn4YZlN6wevFNfQLJOxJTNvoldG83WWZUZi5zYQhX3ZbdADP33B0b9edj9W91O4nsQu-sn-NUNn~WXUb1B-r5Ayd4EsERxM~THenTC8pIHcLGv9i-kx6ACzS8xCdrTbnjh6HGfxZyAMTFh-52jR8cPAFIE8Kdqu5W1Ws3CCtkKFH9lQ3WEZxYpxO4sc2qHEpyDka3gF3wj7YTfxf4kr1-zBT5hr8aG2NUJ7f9KG9eRm8aV4p761ebUoqfvVSSlwUJLwx8XtEU0wouOklD7RZ4wvp1PnL-ntaO3MkxxMyjlBYo8~fVQj1Q__",
+    description:
+      "Sarga Printing is a leader in offset printing, combining cutting-edge technology with exceptional craftsmanship to deliver top-quality prints. With a strong legacy of trust and innovation,",
+    link: "http://64.227.153.239:8000/",
+  },
+  {
+    img: "https://media-hosting.imagekit.io//929e762ebbf44e5d/odredge.png?Expires=1834399536&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=XJEk~pJBKFoo1sk85E1jADE5F3co0RXVyNKfvNzrNNIKi995BW~nUQelcn~HbNYMjGB54yNNPS1h~Jj2VXe~m6L80dv0qxFjY9ToxPataS2ulMTMl6NURjTIS1tyijd4eBF8McplqjV4e1J9OtlVwmoCr5PqQFMZQ-JjuQmy1TDJrfA3R6OVXt9CoAimLm9dC7rGIrdQ43Qz6t8AzTHeDNsz~UAgCjT9EFZypKRJwji-XGLzXw9fMm9Gqaf5gcGeP49jNDq3Q3XiPL9BjkN1trN1q1f3PXogyu9CfkhuYs-M998SB9NSQqxXsilTizlkVa~I6BgHd6YiMneq4gZoLg__",
+    description: "The platform that seamlessly facilitates Online.",
+    link: "https://adredge.com/",
+  },
+  {
+    img: "https://media-hosting.imagekit.io//aac15225844e4666/bank.jpeg?Expires=1834400269&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=2hp0Kq3ymJ3-t8uLCvyNJuGW5y54RNNj-K~zDbGxOmbzAe8fZ3QpFoKwYyEKoUnJZRzFC8HfC4FMLhIYfBMTQuueeepdTVDxtASJsA-Q7r2WuxAglRovjyBaeOOaYCMBOSXJj--Qe-2z2tFU6O1hn6xAzV60~Ap6TYxhsuxKsDwtrjnDu8mPR~LOPS76iKB5YdoORr1GcASOpchz5GQZOrpZ5J0b96lM4T-zFHxklw57A1P-oNwAhQhjJqLyRvtKP-gYYA82CRamz0LBOLiqcQsX0H2~WbwEBA3dmb2iGr0qZHamxLZ8eB7LKWZ1F4n59Eea-SurlYxWrIZYav8J2w__",
+    description: "Basic bank operation application.",
+    code: "https://github.com/ARSHADMSC/BankApplication",
+  },
+  {
+    img: "https://media-hosting.imagekit.io//433ff8f597aa4a5a/alcohol.png?Expires=1834401941&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=U55bXMLXWg~zzFUiFLuuI9frb2HI~hSS~GaDtE8KNvmSu7G7OXnxZNI~rdrS8uPfvBqXUt39bMkd~FJ1suzW8AEXJkr-VSWynNBG9sr0WGLB697jEFvyZ5jLWHMcu1zazUMvFKc84k7D-6zDjnpy09XcGiEsIXO1bUTiQO736aN2C14aE78m6Vq~AZqKnepy-l5ahJdr1pvGCa0gpwm7Jak7YlKonz64BWa4SxGWUl-oeRM~qZPRTQjHmrXqzls5ZnbQBsvCvNuC5l-k6B2Ug-jNAsyei4kQ4T9~3txSqX6HCP2OjKe9Vh-S5B4dQMrmNKf9OjzWGXm-fr7Ba038TA__",
+    description: "Alcohol Consumption in Russia using R.",
+    code: "https://github.com/ARSHADMSC/Alcohol_Consumption",
+  },
+  {
+    img: "https://media-hosting.imagekit.io//ed31e628303e4c3b/placemntdata.jpeg?Expires=1834403501&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=Brj0bW0MOFRjklFRNpeqnUuApQAsPBKPB1rnDMahRELK55hTmviawMZH77LjaBBdrZyQI7OaPUNaSdXwJX5anCyp5K~lb-wRArpslee6VntlikBo~cdrfxAQAtl9PTvBQWVrHQ~WyFcuqwV65J-pZzvOkz1~9lmKLuZHk66UaOTS~SoCb6OR1wlY1YghW-TTuyfLAy7QkXKDpWiB5rP4OBraZ0U1cvKzhKB74uAw5VHhyDtab6CSIusQTSpFhtiblDenUwlovzd5dJ1OCzUPWpTHB~Mm~TlNFGWLYKqoOOOLpTXvoOxsK04GKdLvANGFNgiJ6m0Yw3aLu1kj~lu~Cw__",
+    description: "Real time placement data using R",
+    code: "https://github.com/ARSHADMSC/Placement_data",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "mkarshad099@gmail.com",
+  YOUR_FONE: "9744658488",
+  description:
+    "I’d love to connect and collaborate! Whether you have a project idea, a question, or just want to say hello, feel free to reach out. I am always open to new opportunities and discussions in  Web Development, Full Stack Development, and Data Science.",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_wougpkf",
+  YOUR_TEMPLATE_ID: "template_g4knf0i",
+  YOUR_USER_ID: "j62ZBx7EuSV3fkfrW",
+};
+
+const socialprofils = {
+  github: "https://github.com/ARSHADMSC",
+  // facebook: "https://facebook.com",
+  linkedin: "https://www.linkedin.com/in/arshad-mk-25366a221/",
+  instagram: "https://www.instagram.com/?hl=en",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  qualification,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
